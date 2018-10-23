fce5a809d48bb64435f35cf90aa07ac6313cf7c4
Fulmen545 <richard.janitor@gmail.com>
2018-10-23 21:32:37 +0200

    Settings layout changed

--- app/src/main/java/com/riso/android/mealtracker/SettingsActivity.java
+++ app/src/main/java/com/riso/android/mealtracker/SettingsActivity.java
@@ -1,15 +1,11 @@
 package com.riso.android.mealtracker;
 
 import android.content.ContentValues;
-import android.content.Intent;
 import android.database.Cursor;
-import android.os.PersistableBundle;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
